5fc9d5386aa97f23b6c1d50335183ddaa618055b
Fariha Promi <farihapromi2611@gmail.com>
2025-05-18 11:25:51 +0600

    Submit form

--- src/components/SimpleForm/SimpleForm.jsx
+++ src/components/SimpleForm/SimpleForm.jsx
@@ -2,13 +2,19 @@ import React from 'react'
 
 const SimpleForm = () => {
   const handleClick=e=>{
+    e.preventDefault();
+    console.log(e.target.name.value)
+    console.log(e.target.email.value)
     console.log('form submitted')
   }
   return (
     <div>
       <form onSubmit={handleClick}>
         <label htmlFor="">Name</label>
-        <input type="text" name="" id="" />
+        <input type="text" name="name" id="" />
+        <br />
+        <label htmlFor="">Email</label>
+              <input type="text" name="email" id="" />
         <br />
       <input type="submit" value="Submit" />
       </form>
